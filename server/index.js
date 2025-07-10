9f5d4ea89e84de1c78329a14f5bc5bbe1ba23244
PawanPawar11 <pawanpawar4369@gmail.com>
2025-07-11 00:26:05 +0530

    Added new URL given by vercel in the cors

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://basic-react-chat-app-using-socket-i.vercel.app",
+    ],
   },
 });
 
